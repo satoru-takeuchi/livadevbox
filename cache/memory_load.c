ecf840497bb3bd5fd0037798eedecff3e65db632
satoru.takeuchi@gmail.com <satoru.takeuchi@gmail.com>
2018-03-10 06:34:05 +0900

    add memory_load

--- cache/memory_load.c
+++ cache/memory_load.c
@@ -6,15 +6,6 @@
 #include <err.h>
 
 #define CACHE_LINE_SIZE	64
-#define NLOOP		(64*1024*1024)
-
-#define NSECS_PER_SEC	1000000000UL
-
-static inline long diff_nsec(struct timespec before, struct timespec after)
-{
-        return ((after.tv_sec * NSECS_PER_SEC + after.tv_nsec)
-                - (before.tv_sec * NSECS_PER_SEC + before.tv_nsec));
-}
 
 int main(int argc, char *argv[])
 {
@@ -43,16 +34,12 @@ int main(int argc, char *argv[])
 	clock_gettime(CLOCK_MONOTONIC, &before);
 
 	int i;
-	for (i = 0; i < NLOOP / (size / CACHE_LINE_SIZE); i++) {
+	for (;;) {
 		long j;
 		for (j = 0; j < size; j += CACHE_LINE_SIZE)
 			buffer[j] = 0;
 	}
 
-        clock_gettime(CLOCK_MONOTONIC, &after);
-
-	printf("%f\n", (double)diff_nsec(before, after) / NLOOP);
-	
 	if (munmap(buffer, size) == -1)
 		err(EXIT_FAILURE, "munmap() failed");
 	exit(EXIT_SUCCESS);
